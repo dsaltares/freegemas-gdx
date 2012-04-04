b2ce2bd08b70f35e235b04b5edbdba1844d4fb3c
siondream <david.saltares@gmail.com>
2012-04-04 00:04:35 +0000

    !A - Adds mouse cursor (shows on all platforms)

--- freegemas-desktop/src/com/siondream/freegemas/Freegemas.java
+++ freegemas-desktop/src/com/siondream/freegemas/Freegemas.java
@@ -4,14 +4,30 @@ import java.util.HashMap;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Freegemas implements ApplicationListener {
+	
+	// States
 	private HashMap<String, State> _states = null;
 	private State _currentState = null;
 	
+	// Assets
+	private AssetManager _assetManager = null;
+	
+	private SpriteBatch _batch = null;
+	
+	// Mouse pointer
+	private Texture _mouseTexture = null;
+	
 	@Override
 	public void create() {
+		// Create assets manager
+		_assetManager = new AssetManager();
+		
 		// Create states table
 		_states = new HashMap<String, State>();
 		
@@ -20,11 +36,24 @@ public class Freegemas implements ApplicationListener {
 		// Asign initial state
 		
 		// Load initial state
+		
+		// Load general assets
+		_assetManager.load("data/handCursor.png", Texture.class);
+		_assetManager.finishLoading();
+		
+		// Get assets
+		_mouseTexture = _assetManager.get("data/handCursor.png", Texture.class);
+		
+		// Sprite batch
+		_batch = new SpriteBatch();
+		
+		// Mouse hidden
+		Gdx.input.setCursorCatched(true);
 	}
 
 	@Override
 	public void dispose() {
-		
+		_assetManager.dispose();
 	}
 
 	@Override
@@ -37,10 +66,18 @@ public class Freegemas implements ApplicationListener {
 		Gdx.gl.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 		
+		_batch.begin();
+		
+		// Update and render current state
 		if (_currentState != null) {
 			_currentState.update();
 			_currentState.render();
 		}
+		
+		// Render mouse on top
+		_batch.draw(_mouseTexture, Gdx.input.getX(), Gdx.graphics.getHeight() - Gdx.input.getY());
+		
+		_batch.end();
 	}
 
 	@Override
@@ -71,5 +108,9 @@ public class Freegemas implements ApplicationListener {
 		}
 		
 		return false;
+	}
+	
+	public AssetManager getAssetManager() {
+		return _assetManager;
 	}
 }
