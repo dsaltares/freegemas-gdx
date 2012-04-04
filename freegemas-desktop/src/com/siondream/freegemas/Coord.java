cb7410c56e2c51a09b4e486360679aa7845eebb6
siondream <david.saltares@gmail.com>
2012-04-04 06:47:47 +0000

    !A - Adds Coord class

---
+++ freegemas-desktop/src/com/siondream/freegemas/Coord.java
@@ -0,0 +1,16 @@
+package com.siondream.freegemas;
+
+public class Coord {
+	public float x;
+	public float y;
+	
+	public Coord() {
+		x = -1.0f;
+		y = -1.0f;
+	}
+	
+	public Coord(float x, float y) {
+		this.x = x;
+		this.y = y;
+	}
+}
